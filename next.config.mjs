ad163871939b583973f1b12e3cf8ec31463a6870
Runner3545 <runner3545@gmail.com>
2025-10-22 23:53:06 +0300

    fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
-const repo = "pdr-aces";
-
 export default {
-  output: "export",
-  basePath: isProd ? `/${repo}` : "",
-  assetPrefix: isProd ? `/${repo}/` : "",
-  images: { unoptimized: true },
-  trailingSlash: true,
-  env: { NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : "" },
+  images: {},
+  trailingSlash: false,
 };
